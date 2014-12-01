b2d20384851aa6cda492b5eeef3aeddfaa241a72
yuichi matsuoka <anything12new14@gmail.com>
2014-12-01 17:26:36 +0900

    Initial Commit

---
+++ widgetTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  widgetTest
+//
+//  Created by matz on 2014/12/01.
+//  Copyright (c) 2014年 matz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ widgetTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  widgetTest
+//
+//  Created by matz on 2014/12/01.
+//  Copyright (c) 2014年 matz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
